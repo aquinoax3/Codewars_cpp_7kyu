837cafcae44906f0487c0e37ff55b6259d4cc650
Axel Aquino <96102202+aquinoax3@users.noreply.github.com>
2024-03-28 23:35:12 -0400

    Created basic_math_add_or_subtract.cpp

---
+++ basic_math_add_or_subtract.cpp
@@ -0,0 +1,18 @@
+// In this kata, you will do addition and subtraction on a given string. The return value must be also a string.
+
+// Note: the input will not be empty.
+
+// Examples
+// "1plus2plus3plus4"  --> "10"
+// "1plus2plus3minus4" -->  "2"
+
+
+
+#include <string>
+
+std::string calculate(std::string str)
+{
+  // your code here...
+  // enjoy! :)
+  return str;
+}
