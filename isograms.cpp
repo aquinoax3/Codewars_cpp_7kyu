67a19901ba3466f1119c6099e7fb8f5862136fb1
Axel Aquino <96102202+aquinoax3@users.noreply.github.com>
2024-04-10 00:05:52 -0400

    Completed the logic for function

--- isograms.cpp
+++ isograms.cpp
@@ -1,16 +1,3 @@
-// An isogram is a word that has no repeating letters, consecutive or non-consecutive. Implement a function that determines whether a string that contains only letters is an isogram. Assume the empty string is an isogram. Ignore letter case.
-
-// Example: (Input --> Output)
-
-// "Dermatoglyphics" --> true "aba" --> false "moOse" --> false (ignore letter case)
-
-// isIsogram "Dermatoglyphics" = true
-// isIsogram "moose" = false
-// isIsogram "aba" = false
-
-
-
-
 #include <map>
 #include <string>
 #include <algorithm>
@@ -28,6 +15,10 @@ bool is_isogram(std::string str) {
     }
     
     for(auto i = hash.begin(); i != hash.end(); i++){
-        std::cout << i->first << std::endl;
+        if (i->second > 1) {
+        return false;
+        }
     }
+    
+    return true;
 }
