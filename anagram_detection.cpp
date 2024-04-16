96612ff56137a6cd00bf290c541060b0af58a712
Axel Aquino <96102202+aquinoax3@users.noreply.github.com>
2024-04-16 00:13:39 -0400

    Corrected the logic and added comments

--- anagram_detection.cpp
+++ anagram_detection.cpp
@@ -14,32 +14,41 @@
 #include <map>
 #include <algorithm>
 
-bool isAnagram(std::string test, std::string original){
-    std::map<char, int> hash;
-    std::transform(original.begin(), original.end(), original.begin(), ::tolower);
-    std::transform(test.begin(), test.end(), test.begin(), ::tolower);
-    
-    for (auto letter : original) {
-        if (hash.find(letter) == hash.end()) {
-        hash[letter] = 1;
-        } else {
-        hash[letter]++;
-        }
-    }
+bool isAnagram(std::string test, std::string original) {
+    // Create a map to store letters of original string
+        std::map<char, int> hash;
 
-    for(auto letter : test) {
-        if(hash[letter]) {
-        hash[letter]--;
-        } else {
-        if(hash.find(letter) != hash.end()) return false;
+        // transformed orginal and test string to lower case
+        std::transform(original.begin(), original.end(), original.begin(), ::tolower);
+        std::transform(test.begin(), test.end(), test.begin(), ::tolower);
+        
+        // iterate through orginal string to add characters as keys 
+        for (auto letter : original) {
+            // If the iterator returned by find() is equal to hash.end(), it means that letter is NOT FOUND in the map
+            if (hash.find(letter) == hash.end()) {
+            hash[letter] = 1;
+            } else {
+            // if it iterates through the hash and finds the char as a key increase its occurance by 1
+            hash[letter]++;
+            }
         }
-    }
-    
-    for(auto i = hash.begin(); i != hash.end(); i++) {
-        if (i->second > 0) {
-        return false;
+        
+        // iterate through test string and see if the char is found in the hash
+        for(auto letter : test) {
+            // if char is in the hash, decrease the occurance by 1
+            if(hash[letter]) {
+            hash[letter]--;
+            } else {
+            // If the iterator returned by find() is not equal to hash.end(), it means that letter IS FOUND in the map
+            if(hash.find(letter) != hash.end()) return false;
+            }
         }
-    }
-    
-    return true;
+        
+        for(auto i = hash.begin(); i != hash.end(); i++) {
+            if (i->second > 0) {
+            return false;
+            }
+        }
+        
+        return true;
     }
