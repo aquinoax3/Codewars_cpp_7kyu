48e8f0e54bb3fe698354b6b1299f16906d904822
Axel Aquino <96102202+aquinoax3@users.noreply.github.com>
2024-05-08 23:27:34 -0400

    Created string_ends_with.cpp

---
+++ string_ends_with.cpp
@@ -0,0 +1,21 @@
+// Complete the solution so that it returns true if the first argument(string) passed in ends with the 2nd argument (also a string).
+
+// Examples:
+
+// solution('abc', 'bc') // returns true
+// solution('abc', 'd') // returns false
+
+
+
+#include <string>
+
+
+bool solution(std::string const &str, std::string const &ending) {
+    std::string result = str.substr(str.size() - ending.size(), ending.size());
+    
+    std::cout << "og: " << str << std::endl;
+    std::cout << result << std::endl;
+    std::cout << "end: " << ending << std::endl;
+    
+    return result == ending;
+}
