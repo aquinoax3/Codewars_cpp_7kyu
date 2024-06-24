36559dbef3579e9d81ce22586d714fabe82751d1
Axel Aquino <96102202+aquinoax3@users.noreply.github.com>
2024-06-23 23:26:44 -0400

    Create divide_and_conquer.cpp

---
+++ divide_and_conquer.cpp
@@ -0,0 +1,14 @@
+// Given a mixed array of number and string representations of integers, add up the non-string integers and subtract the total of the string integers.
+
+// Return as a number.
+
+
+
+#include <vector>
+#include <variant>
+
+int divCon(const std::vector<std::variant<int, char>>& x) {
+  for (auto num : x) {
+    return num;
+  }
+}
