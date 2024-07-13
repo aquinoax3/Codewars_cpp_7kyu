437a86bcce82ddc45fb3d806f4ce41cad27caf1c
Axel Aquino <96102202+aquinoax3@users.noreply.github.com>
2024-07-12 23:33:05 -0400

    Update all_unique.cpp

--- all_unique.cpp
+++ all_unique.cpp
@@ -12,6 +12,13 @@ bool hasUniqueChars(std::string s) {
     std::unordered_map<char, int> hash;
         
     for(char letter : s) {
+        // if letter is FOUND in the hash map, then it would RETURN an iterator pointing to the key value pair
+        // if the letter is NOT FOUND then hash.find() would returns an iterator equal to "hash.end()"
+        // The if condition essentially checks whether the character letter already exists as a key in the hash map. If it does, the condition is true; otherwise, it's false.
+
+        // If hash.find(letter) == hash.end() in your code, it means the character letter ** IS NOT ** currently present as a key in the hash unordered map.
+        // If hash.find(letter) != hash.end() in your code, it means the character letter ** IS**  currently present as a key in the hash unordered map.
+        
         if(hash.find(letter) != hash.end()){
         hash[letter]++;
         } else{
