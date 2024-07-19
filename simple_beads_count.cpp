312c588d484992629820e7fdad5e5e4aedf3d029
Axel Aquino <96102202+aquinoax3@users.noreply.github.com>
2024-07-18 23:47:26 -0400

    cleaned up the function

--- simple_beads_count.cpp
+++ simple_beads_count.cpp
@@ -6,9 +6,5 @@
 // If there are less than 2 blue beads return 0.
 
 unsigned int countRedBeads( unsigned int n ) {
-    if (n < 2) {
-        return 0;
-    }
-    
-    return (n * 2) - 2;
+    return n < 2 ? 0: (n * 2) - 2;
 }
