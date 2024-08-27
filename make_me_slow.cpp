17a9b2b24cf1863e3ef91e91f1eac079b32753b0
Axel Aquino <96102202+aquinoax3@users.noreply.github.com>
2024-08-27 00:31:20 -0400

    updated function for make_me_slow.cpp

--- make_me_slow.cpp
+++ make_me_slow.cpp
@@ -1,6 +1,11 @@
 // Make me slow! Calling makeMeSlow() should take at least 7 seconds.
 
-void make_me_slow(){
+#include <chrono>
+#include <thread>
+
+void make_me_slow() {
     // This function is too fast!
-    for (int x = 0; x < 100; x++);
+    for (int x = 0; x < 7; x++) {
+        std::this_thread::sleep_for(std::chrono::seconds(1));
+    }
 }
