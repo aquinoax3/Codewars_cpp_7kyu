7a7bd81e39f5f95f41c3861941783360245f5592
Axel Aquino <96102202+aquinoax3@users.noreply.github.com>
2024-10-23 23:32:57 -0400

    Create sum_even_numbers.cpp

---
+++ sum_even_numbers.cpp
@@ -0,0 +1,29 @@
+// Complete the function that takes a sequence of numbers as single parameter. Your function must return the sum of the even values of this sequence.
+
+// Only numbers without decimals like 4 or 4.0 can be even.
+
+// The input is a sequence of numbers: integers and/or floats.
+
+// Examples
+// [4, 3, 1, 2, 5, 10, 6, 7, 9, 8]  -->  30   # because 4 + 2 + 10 + 6 + 8 = 30
+// []                               -->  0
+
+
+
+
+#include <vector>
+#include <cmath>
+
+int sum_even_numbers(const std::vector<double> &seq) {
+  int sum = 0;
+  
+  for (auto num : seq) {
+    int whole_num = num;
+    double dub_num = whole_num - num;
+    if(whole_num % 2 == 0 && dub_num < 0) {
+      sum += whole_num;
+    }
+  }
+  
+  return sum;
+}
