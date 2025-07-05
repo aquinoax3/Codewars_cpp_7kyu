f1bb6e39131372f4c71b0f232e184179c1585c1d
Axel Aquino <96102202+aquinoax3@users.noreply.github.com>
2025-07-05 18:55:33 -0400

    Create fun_with_lists_length.cpp

---
+++ fun_with_lists_length.cpp
@@ -0,0 +1,34 @@
+// Implement the method length, which accepts a linked list (head), and returns the length of the list.
+
+// For example: Given the list: 1 -> 2 -> 3 -> 4, length should return 4.
+
+// The linked list is defined as follows:
+
+// template <class T>
+// class Node
+// {
+// public:
+//     T data;
+//     Node *next;
+//     Node(T x, Node *p = NULL) : data(x), next(p) {}
+//     Node() : next(NULL) {}
+// };
+// Note: the list may be null and can hold any type of value.
+
+// Good luck!
+
+template <class T>
+int length(const Node<T> *head) {
+  int count = 0;
+  if (head == nullptr) {
+    return count;
+  }
+  
+  const Node<T>* curr = head;
+  
+  while (curr != nullptr) {
+    count++;
+    curr = curr->next;
+  }
+    return count;
+}
