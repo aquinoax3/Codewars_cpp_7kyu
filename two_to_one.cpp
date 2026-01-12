41b5adaafd0a842e794da2a2a38d6c5436116e5c
Axel Aquino <96102202+aquinoax3@users.noreply.github.com>
2026-01-11 23:30:55 -0500

    Update two_to_one.cpp

--- two_to_one.cpp
+++ two_to_one.cpp
@@ -8,3 +8,26 @@
 // a = "abcdefghijklmnopqrstuvwxyz"
 // longest(a, a) -> "abcdefghijklmnopqrstuvwxyz"
 
+
+#include <unordered_set>
+#include <string>
+#include <algorithm>
+
+class TwoToOne {
+public:
+    static std::string longest(const std::string &s1, const std::string &s2) {
+      std::string fullStr = s1 + s2;
+      std::unordered_set<char> noDupes(fullStr.begin(), fullStr.end());
+  
+      std::string result = "";
+      for (char ch : noDupes) {
+        result += ch;
+      }
+      
+      std::sort(result.begin(), result.end());
+      
+      return result;
+    }
+    
+    
+};
