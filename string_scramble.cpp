cf9c7272e268128d41454df82ac4cb4532289d42
Axel Aquino <96102202+aquinoax3@users.noreply.github.com>
2026-01-17 20:21:16 -0500

    Create string_scramble.cpp

---
+++ string_scramble.cpp
@@ -0,0 +1,35 @@
+// Given a string and an array of indices, rearrange the characters of the string so that each character is placed at the position specified by the corresponding index in the array.
+
+// Example
+
+// input: "abcd", [0, 3, 1, 2]
+// output: "acdb"
+// Explanation
+
+// The character 'a' is placed at index 0.
+
+// The character 'b' is placed at index 3.
+
+// The character 'c' is placed at index 1.
+
+// The character 'd' is placed at index 2.
+
+// Notes
+
+// The string and the array will be of equal length.
+
+// The string will contain valid characters (A-Z, a-z, or 0-9);
+// the array will contain valid indices.
+
+
+#include <string>
+#include <vector>
+#include <cstddef>
+
+std::string scramble(
+  const std::string& str,
+  const std::vector<std::size_t>& indices
+) {
+  // write code here.
+  return str;
+}
