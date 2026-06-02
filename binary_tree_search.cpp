4ef7e04ddd065f0f9af965d2d416e6a1dd96c2fd
Axel Aquino <96102202+aquinoax3@users.noreply.github.com>
2026-06-02 19:14:25 -0400

    Update binary_tree_search.cpp

--- binary_tree_search.cpp
+++ binary_tree_search.cpp
@@ -4,12 +4,40 @@
 // return False/false if it isn't
 
 
+#include <stack>
+#include <iostream>
+
 struct Node{
   int val;
   Node *left = nullptr;
   Node *right = nullptr;
 };
 
+
 bool search(int n, Node *root){
-  //Your code goes here
+  if (root == nullptr) {
+    return false;
+  }
+  
+  std::stack<Node*> stack;
+  stack.push(root);
+  
+  while (!stack.empty()) {
+    Node* curr = stack.top();
+    stack.pop();
+    
+    if (curr->val == n) {
+      return true;
+    }
+    
+    if (curr->right != nullptr) {
+      stack.push(curr->right);
+    }
+    
+    if (curr->left != nullptr) {
+      stack.push(curr->left);
+    }
+  }
+  
+  return false;
 }
