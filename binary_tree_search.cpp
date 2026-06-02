155c78fd58062755fb41700da7fbd30c4e892cf8
Axel Aquino <96102202+aquinoax3@users.noreply.github.com>
2026-06-01 22:41:48 -0400

    Create binary_tree_search.cpp

---
+++ binary_tree_search.cpp
@@ -0,0 +1,15 @@
+// Given a number and a binary tree ( not a Binary Search Tree! ):
+
+// return True/true if the given number is in the tree
+// return False/false if it isn't
+
+
+struct Node{
+  int val;
+  Node *left = nullptr;
+  Node *right = nullptr;
+};
+
+bool search(int n, Node *root){
+  //Your code goes here
+}
