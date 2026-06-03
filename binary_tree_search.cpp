7362d7b7fe11fa183471da9e1a007a8c3634dd4d
Axel Aquino <96102202+aquinoax3@users.noreply.github.com>
2026-06-03 18:38:17 -0400

    Update binary_tree_search.cpp

--- binary_tree_search.cpp
+++ binary_tree_search.cpp
@@ -41,3 +41,11 @@ bool search(int n, Node *root){
   
   return false;
 }
+
+
+// Recursive Solution
+bool searchRecursive(int n, Node *root){
+  if (root == nullptr) return false;
+  if (root->val == n) return true;
+  return search(n, root->left) || search(n, root->right);
+}
