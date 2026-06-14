3c8050ed57ccb747af477e1018cf5e0cec8ea42f
Axel Aquino <96102202+aquinoax3@users.noreply.github.com>
2026-06-13 20:12:08 -0400

    Create consecutive_letters.cpp

---
+++ consecutive_letters.cpp
@@ -0,0 +1,36 @@
+// In this Kata, we will check if a string contains consecutive letters as they appear in the English alphabet and if each letter occurs only once.
+
+// Rules are: (1) the letters are adjacent in the English alphabet, and (2) each letter occurs only once.
+
+// For example: 
+// solve("abc") = True, because it contains a,b,c
+// solve("abd") = False, because a, b, d are not consecutive/adjacent in the alphabet, and c is missing.
+// solve("dabc") = True, because it contains a, b, c, d
+// solve("abbc") = False, because b does not occur once.
+// solve("v") = True
+// All inputs will be lowercase letters.
+
+// More examples in test cases. Good luck!
+
+
+
+#include <iostream>
+#include <unordered_set>
+
+bool solve(std::string s) {
+	if (s.size() == 1) {
+    return true;
+  }
+  
+  std::unordered_set<char> seen;
+  
+  for (int i = 0; i < s.size(); i++) {
+    if (s.find(s[i]) != s.end()) {
+      return false;
+    }
+    
+  }
+  
+  
+  return false
+}
