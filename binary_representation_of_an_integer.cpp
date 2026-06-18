84d3844d229b34203df7749b36608b529ae46b23
Axel Aquino <96102202+aquinoax3@users.noreply.github.com>
2026-06-17 20:20:10 -0400

    Create binary_representation_of_an_integer.cpp

---
+++ binary_representation_of_an_integer.cpp
@@ -0,0 +1,16 @@
+// Write a function which takes an argument n, (a signed 32-bit integer in 2s-complement format) and returns an array of integers of length 32, containing the bits of n. (Least Significant Bit on the right).
+
+// Assign either a 1 or a 0 to the array element depending on whether the bit at the corresponding position is a 1 or 0.
+
+// For example (input -> output):
+
+//  1 -> [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1]
+// -1 -> [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1]
+
+
+
+#include <vector>
+
+std::vector<int> showBits(int n){
+  return { };
+}
